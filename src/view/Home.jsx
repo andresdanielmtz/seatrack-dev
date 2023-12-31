9e2ef4fe8bdf2a32aef2734cbc9742ab62f9319d
Andrés Martínez <autummata@gmail.com>
2023-12-30 22:50:21 -0700

    removed unused getCoords()

--- src/view/Home.jsx
+++ src/view/Home.jsx
@@ -1,37 +1,13 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import axios from "axios";
-
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 function MainView({ username }) {
-  const [coords, setCoords] = useState(null);
-  const [showCoords, setShowCoords] = useState(false);
   const [latitude, setLatitude] = useState("");
   const [longitude, setLongitude] = useState("");
   const [name, setName] = useState("");
-  const [content, setContent] = useState([]);
-
-  function getCoords() {
-    axios({
-      method: "GET",
-      url: "/coords",
-    })
-      .then((response) => {
-        const res = response.data;
-        const names = res.map((item) => item.name); // get only the names
-        setContent(names);
-        console.log(names);
-      })
-      .catch((error) => {
-        if (error.response) {
-          console.log(error.response);
-          console.log(error.response.status);
-          console.log(error.response.headers);
-        }
-      });
-  }
 
   function uploadCoords() {
     axios({
@@ -45,7 +21,6 @@ function MainView({ username }) {
     })
       .then((response) => {
         console.log(response);
-        getCoords();
         toast.success("Coordinates uploaded successfully!"); // Show success notification
       })
       .catch((error) => {
@@ -54,15 +29,6 @@ function MainView({ username }) {
       });
   }
 
-  function buttonCoords() {
-    getCoords();
-    setShowCoords(!showCoords);
-  }
-
-  useEffect(() => {
-    getCoords();
-  }, []);
-
   return (
     <div>
       <h3> Welcome, {username} </h3>
@@ -70,7 +36,6 @@ function MainView({ username }) {
         Sí esto muestra la información, el backend se conectó con el frontend:{" "}
       </i>{" "}
       <br />
-      <button onClick={buttonCoords}> Get Coordinates </button>
       <h3> Upload Coordinates </h3>
       <div>
         <input
